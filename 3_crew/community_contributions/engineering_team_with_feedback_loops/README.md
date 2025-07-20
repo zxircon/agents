662247eccb47307e724e51430363febc5cce9e68
Vikram Vasudevan <vasudevan.vikram@gmail.com>
2025-07-20 18:20:37 +0530

    Added UI

--- 3_crew/community_contributions/engineering_team_with_feedback_loops/README.md
+++ 3_crew/community_contributions/engineering_team_with_feedback_loops/README.md
@@ -1,3 +1,9 @@
+---
+title: Engineering_Team_of_Agents
+app_file: app.py
+sdk: gradio
+sdk_version: 5.38.0
+---
 # Engineering Crew
 
 Welcome to theEngineering Crew project, powered by [crewAI](https://crewai.com). This template is designed to help you set up a multi-agent AI system with ease, leveraging the powerful and flexible framework provided by crewAI. Our goal is to enable your agents to collaborate effectively on complex tasks, maximizing their collective intelligence and capabilities.
--- 3_crew/community_contributions/engineering_team_with_feedback_loops/src/engineering_team_using_flow/main.py
+++ 3_crew/community_contributions/engineering_team_with_feedback_loops/src/engineering_team_using_flow/main.py
@@ -12,6 +12,7 @@ from engineering_team_using_flow.crews.engineering_crew.engineering_crew import 
     CodeReviewFeedback,
     EngineeringCrew,
 )
+from .shared_queue import TaskInfo, add_to_queue
 
 
 class EngineeringState(BaseModel):
@@ -23,6 +24,7 @@ class EngineeringState(BaseModel):
     unit_test_code: Optional[str] = ""
     backend_code_review_feedbacks: list[CodeReviewFeedback] = []
     frontend_code_review_feedbacks: list[CodeReviewFeedback] = []
+
 
 MAX_REVIEW_ITERATIONS = 3
 BUSINESS_REQUIREMENTS: list[EngineeringState] = [
@@ -37,16 +39,47 @@ BUSINESS_REQUIREMENTS: list[EngineeringState] = [
 
 class EngineeringFlow(Flow[EngineeringState]):
 
+    def __init__(self, module_name:str, business_requirement:str):
+        super().__init__()
+        self.module_name = module_name
+        self.business_requirement = business_requirement
+
     @start()
     def generate_business_requirement(self):
         print("Generating business requirement")
-        chosen_requirement = random.choice(BUSINESS_REQUIREMENTS)
-        self.state.business_requirement = chosen_requirement.business_requirement
-        self.state.module_name = chosen_requirement.module_name
+        self.state.business_requirement = self.business_requirement
+        self.state.module_name = self.module_name
+
+        add_to_queue(
+            TaskInfo(
+                name="Gathering Business Requirements",
+                type="markdown",
+                output=f"Gathering Business Requirements for {self.state.module_name}...",
+            )
+        )
+
+        # chosen_requirement = random.choice(BUSINESS_REQUIREMENTS)
+        # self.state.business_requirement = chosen_requirement.business_requirement
+        # self.state.module_name = chosen_requirement.module_name
+        add_to_queue(
+            TaskInfo(
+                name="Gather Business Requirements",
+                type="markdown",
+                output=f"#### Business Requirements for {self.state.module_name}: \n{self.state.business_requirement}",
+            )
+        )
 
     @listen(generate_business_requirement)
     def design_product(self):
         print("Designing product for requirement: ", self.state)
+        add_to_queue(
+            TaskInfo(
+                name="Generating Design",
+                type="markdown",
+                output=f"Designing the product ...",
+            )
+        )
+
         engineeringCrew = EngineeringCrew()
 
         mycrew = Crew(
@@ -55,19 +88,33 @@ class EngineeringFlow(Flow[EngineeringState]):
         )
         result = mycrew.kickoff(
             inputs={
-                "id" : self.state.id,
+                "id": self.state.id,
                 "requirement": self.state.business_requirement,
                 "module_name": self.state.module_name,
-                "review_comments" : ""
+                "review_comments": "",
             }
         )
 
         print("Product Design Created", result.raw)
         self.state.technical_design = result.raw
+        add_to_queue(
+            TaskInfo(
+                name="Generate Design",
+                type="markdown",
+                output=f"{self.state.technical_design}",
+            )
+        )
 
-    @listen(or_("design_product","REWRITE_BACKEND_CODE"))
+    @listen(or_("design_product", "REWRITE_BACKEND_CODE"))
     def develop_backend(self):
         print("Developing backend : ", self.state)
+        add_to_queue(
+            TaskInfo(
+                name="Generating Backend Code",
+                type="markdown",
+                output=f"Generating Backend Code ...",
+            )
+        )
         engineeringCrew = EngineeringCrew()
 
         mycrew = Crew(
@@ -76,23 +123,50 @@ class EngineeringFlow(Flow[EngineeringState]):
         )
         result = mycrew.kickoff(
             inputs={
-                "id" : self.state.id,
+                "id": self.state.id,
                 "requirement": self.state.business_requirement,
                 "module_name": self.state.module_name,
-                "review_comments" : self.state.backend_code_review_feedbacks[-1].review_comments_markdown if self.state.backend_code_review_feedbacks else ""
+                "review_comments": (
+                    self.state.backend_code_review_feedbacks[
+                        -1
+                    ].review_comments_markdown
+                    if self.state.backend_code_review_feedbacks
+                    else ""
+                ),
             }
         )
 
         print("Backend Code Created", result.raw)
         self.state.backend_code = result.raw
+        add_to_queue(
+            TaskInfo(
+                name="Generate Backend Code",
+                type="markdown",
+                output=f"#### Backend Code for {self.state.module_name}: \n ```{self.state.backend_code}\n```",
+            )
+        )
         return "BACKEND_CODE_CREATED"
 
     @router(develop_backend)
     def review_backend_code(self):
         print("Reviewing backend code : ", self.state)
-        if(len(self.state.backend_code_review_feedbacks) >= MAX_REVIEW_ITERATIONS):
+        if len(self.state.backend_code_review_feedbacks) >= MAX_REVIEW_ITERATIONS:
+            add_to_queue(
+                TaskInfo(
+                    name="Maximum Iterations Exceeded!",
+                    type="markdown",
+                    output=f"#### Maximum Iterations Exceeded!",
+                )
+            )
             return "MAX_REVIEW_ITERATIONS_EXCEEDED"
         engineeringCrew = EngineeringCrew()
+        add_to_queue(
+            TaskInfo(
+                name="Reviewing Backend Code",
+                type="markdown",
+                output=f"Reviewing Backend Code ...",
+            )
+        )
 
         mycrew = Crew(
             agents=[engineeringCrew.code_reviewer()],
@@ -103,20 +177,26 @@ class EngineeringFlow(Flow[EngineeringState]):
 
         result = mycrew.kickoff(
             inputs={
-                "id" : self.state.id,
+                "id": self.state.id,
                 "requirement": self.state.business_requirement,
                 "module_name": self.state.module_name,
                 "backend_code": self.state.backend_code,
-                "iteration" : len(self.state.backend_code_review_feedbacks)
-                
+                "iteration": len(self.state.backend_code_review_feedbacks),
             }
         )
 
-        codeReviewFeedback : CodeReviewFeedback = result.tasks_output[0].pydantic # type: ignore[index]
+        codeReviewFeedback: CodeReviewFeedback = result.tasks_output[0].pydantic  # type: ignore[index]
         print(codeReviewFeedback)
-        self.state.backend_code_review_feedbacks.append(codeReviewFeedback) 
+        add_to_queue(
+            TaskInfo(
+                name="Generate Backend Code Review",
+                type="markdown",
+                output=f"#### Backend Code Review Iteration {len(self.state.backend_code_review_feedbacks)} for {self.state.module_name}: \n{codeReviewFeedback.review_comments_markdown}",
+            )
+        )
+        self.state.backend_code_review_feedbacks.append(codeReviewFeedback)
 
-        if(codeReviewFeedback.passed_review):
+        if codeReviewFeedback.passed_review:
             return "BACKEND_CODE_REVIEWED"
         else:
             return "REWRITE_BACKEND_CODE"
@@ -124,6 +204,13 @@ class EngineeringFlow(Flow[EngineeringState]):
     @listen(or_("BACKEND_CODE_REVIEWED", "REWRITE_FRONTEND_CODE"))
     def develop_frontend(self):
         print("Developing frontend : ", self.state)
+        add_to_queue(
+            TaskInfo(
+                name="Developing Frontend Code",
+                type="markdown",
+                output=f"Developing Frontend Code ...",
+            )
+        )
         engineeringCrew = EngineeringCrew()
 
         mycrew = Crew(
@@ -132,24 +219,50 @@ class EngineeringFlow(Flow[EngineeringState]):
         )
         result = mycrew.kickoff(
             inputs={
-                "id" : self.state.id,
+                "id": self.state.id,
                 "requirement": self.state.business_requirement,
                 "module_name": self.state.module_name,
-                "review_comments" : self.state.frontend_code_review_feedbacks[-1].review_comments_markdown if self.state.frontend_code_review_feedbacks else ""
+                "review_comments": (
+                    self.state.frontend_code_review_feedbacks[
+                        -1
+                    ].review_comments_markdown
+                    if self.state.frontend_code_review_feedbacks
+                    else ""
+                ),
             }
         )
 
         print("Frontend Code Created", result.raw)
         self.state.frontend_code = result.raw
+        add_to_queue(
+            TaskInfo(
+                name="Generate Frontend Code",
+                type="markdown",
+                output=f"#### Frontend Code for {self.state.module_name}:\n ```{self.state.frontend_code}```",
+            )
+        )
         return "FRONTEND_CODE_CREATED"
-
 
     @router(develop_frontend)
     def review_frontend_code(self):
         print("Reviewing frontkend code : ", self.state)
-        if(len(self.state.frontend_code_review_feedbacks) >= MAX_REVIEW_ITERATIONS):
+        if len(self.state.frontend_code_review_feedbacks) >= MAX_REVIEW_ITERATIONS:
+            add_to_queue(
+                TaskInfo(
+                    name="Maximum Iterations Exceeded!",
+                    type="markdown",
+                    output=f"#### Maximum Iterations Exceeded!",
+                )
+            )
             return "MAX_REVIEW_ITERATIONS_EXCEEDED"
         engineeringCrew = EngineeringCrew()
+        add_to_queue(
+            TaskInfo(
+                name="Reviewing Frontend Code",
+                type="markdown",
+                output=f"Reviewing Frontend Code ...",
+            )
+        )
 
         mycrew = Crew(
             agents=[engineeringCrew.code_reviewer()],
@@ -160,19 +273,26 @@ class EngineeringFlow(Flow[EngineeringState]):
 
         result = mycrew.kickoff(
             inputs={
-                "id" : self.state.id,
+                "id": self.state.id,
                 "requirement": self.state.business_requirement,
                 "module_name": self.state.module_name,
                 "frontend_code": self.state.frontend_code,
-                "iteration" : len(self.state.frontend_code_review_feedbacks)
+                "iteration": len(self.state.frontend_code_review_feedbacks),
             }
         )
 
-        codeReviewFeedback : CodeReviewFeedback = result.tasks_output[0].pydantic # type: ignore[index]
+        codeReviewFeedback: CodeReviewFeedback = result.tasks_output[0].pydantic  # type: ignore[index]
         print(codeReviewFeedback)
-        self.state.frontend_code_review_feedbacks.append(codeReviewFeedback) 
+        add_to_queue(
+            TaskInfo(
+                name="Generate Frontend Code Review",
+                type="markdown",
+                output=f"#### Frontend Code Review Iteration {len(self.state.frontend_code_review_feedbacks)} for {self.state.module_name}: \n{codeReviewFeedback.review_comments_markdown}",
+            )
+        )
+        self.state.frontend_code_review_feedbacks.append(codeReviewFeedback)
 
-        if(codeReviewFeedback.passed_review):
+        if codeReviewFeedback.passed_review:
             return "FRONTEND_CODE_REVIEWED"
         else:
             return "REWRITE_FRONTEND_CODE"
@@ -180,6 +300,13 @@ class EngineeringFlow(Flow[EngineeringState]):
     @listen("FRONTEND_CODE_REVIEWED")
     def write_test_cases(self):
         print("Writing test cases : ", self.state)
+        add_to_queue(
+            TaskInfo(
+                name="Writing Test Cases",
+                type="markdown",
+                output=f"Writing Test Cases ...",
+            )
+        )
         engineeringCrew = EngineeringCrew()
 
         mycrew = Crew(
@@ -188,15 +315,23 @@ class EngineeringFlow(Flow[EngineeringState]):
         )
         result = mycrew.kickoff(
             inputs={
-                "id" : self.state.id,
+                "id": self.state.id,
                 "requirement": self.state.business_requirement,
                 "module_name": self.state.module_name,
                 "backend_code": self.state.backend_code,
-                "frontend_code": self.state.frontend_code
+                "frontend_code": self.state.frontend_code,
             }
         )
 
         self.state.unit_test_code = result.raw
+        add_to_queue(
+            TaskInfo(
+                name="Generate Test Cases",
+                type="markdown",
+                output=f"#### Test Cases for {self.state.module_name}: \n```{self.state.unit_test_code}```",
+            )
+        )
+
         return "TEST_CASES_PREPARED"
 
 
